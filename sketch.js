4a9ad74fcc6b9b61d22975976549ae03d06e4856
KYTO1234 <79738839+KYTO1234@users.noreply.github.com>
2021-08-08 17:53:07 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -61,7 +61,7 @@ function setup() {
   polygon=Bodies.circle(50,200,20);
   World.add(world,polygon);
 
-slingShot=new SlingShot(this.polygon,{x:100,y:200});
+slingShot=new Slingshot(this.polygon,{x:100,y:200});
 
 }
 
